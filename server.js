773e623ec2b6f84391a41b8354efad8f6f29673e
Debbie Hall <debleehall15@gmail.com>
2022-02-21 20:43:20 -0500

    date format fixed

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ const sess = {
 
 app.use(session(sess));
 
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
-const hbs = exphbs.create({});
+// const hbs = exphbs.create({});
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
